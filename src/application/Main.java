4fdd394e0f5da34fc66d66db8d43f724c540a23c
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-13 20:59:39 -0300

    wildcard types

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,34 +1,18 @@
 package application;
 
-import entities.Product;
-import services.CalculationService;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        final String FILE_PATH = "/home/leonardords/temp/in.csv";
+        List<Integer> numbers = Arrays.asList(5, 2, 10);
+        printList(numbers);
 
-        final List<Product> products = new ArrayList<>();
+        List<String> names = Arrays.asList("Maria", "Alex", "Bob");
+        printList(names);
+    }
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_PATH))) {
-            String line = bufferedReader.readLine();
-
-            while (line != null) {
-                String[] fields = line.split(",");
-                products.add(new Product(fields[0], Double.parseDouble(fields[1])));
-                line = bufferedReader.readLine();
-            }
-
-            System.out.printf("Most expensive: %s", CalculationService.max(products));
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
-        }
+    public static void printList(List<?> anythingList) {
+        System.out.println(anythingList);
     }
 }
