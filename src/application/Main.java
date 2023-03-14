80b421cdb383344c8ea26bc00f2b3455f8c8cb86
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-13 21:44:40 -0300

    bounded wildcards part II

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,33 +1,25 @@
 package application;
 
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Rectangle(3.0, 2.0));
-        shapes.add(new Circle(2.0));
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+        List<Double> doubles = Arrays.asList(3.14, 6.28);
+        List<Object> objects = new ArrayList<>();
 
-        List<Circle> circles = new ArrayList<>();
-        circles.add(new Circle(3.0));
-        circles.add(new Circle(2.0));
-
-        System.out.printf("Total area: %.2f%n", totalArea(shapes));
-        System.out.printf("Total area: %.2f", totalArea(circles));
+        copy(integers, objects);
+        copy(doubles, objects);
+        printList(objects);
     }
 
-    private static double totalArea(List<? extends Shape> shapeList) {
-        double sum = 0.0;
+    public static void copy(List<? extends Number> sourceList, List<? super Object> targetList) {
+        targetList.addAll(sourceList);
+    }
 
-        for (Shape shape : shapeList) {
-            sum += shape.area();
-        }
-
-        return sum;
+    public static void printList(List<?> list) {
+        System.out.println(list);
     }
 }
