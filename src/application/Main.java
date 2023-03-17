2f480fceb921fd1e2df7256b6aabea41858aa611
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-03-16 21:28:43 -0300

    HashSet comparation without equals and hashcode

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,7 @@
 package application;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Main {
 
--- src/application/Product.java
+++ src/application/Product.java
@@ -1,8 +1,7 @@
 package application;
 
-import java.util.Objects;
-
 public class Product {
+
   private String name;
   private Double price;
 
@@ -25,22 +24,5 @@ public class Product {
 
   public void setPrice(Double price) {
     this.price = price;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Product product = (Product) o;
-    return Objects.equals(name, product.name) && Objects.equals(price, product.price);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, price);
   }
 }
